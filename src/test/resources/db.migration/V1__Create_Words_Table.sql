b33a072a9321b51f120745902578a6d578262f5a
abdoulayedrame <drame.berlin@gmail.com>
2023-07-02 21:50:22 +0200

    corrects testing.

--- src/test/resources/db.migration/V1__Create_Words_Table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE cgm.words
-(
-    id         BIGSERIAL PRIMARY KEY,
-    word       VARCHAR(255) UNIQUE NOT NULL, --make word UNIQUE
-    is_premium BOOLEAN                       -- premium or not
-);
